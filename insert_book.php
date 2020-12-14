8862cd3313173f68b0e57720848b8e66926e230b
Simweixian <66108179+Simweixian@users.noreply.github.com>
2020-12-14 15:26:33 +0800

    Add files via upload

---
+++ insert_book.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Entry Results</title>
+</head>
+<body>
+    <h1>Book Entry Results</h1>
+    <?php
+    // TODO 1: Create short variable names.
+       $lh='localhost';
+       $db='publications';
+       $un='sim';
+       $pw='sim';
+
+
+    // TODO 2: Check and filter data coming from the user.
+      if(isset($_POST['isbn'])){
+          $isbn = $_POST['isbn'];
+          $author = $_POST['author'];
+          $title = $_POST['title'];
+          $price = $_POST['price'];
+      }else{
+          echo "no data";
+      }
+
+    // TODO 3: Setup a connection to the appropriate database.
+       $conn = new mysqli($lh,$un,$pw,$db);
+       if($conn->connect_error){
+           die ("databases unable to connect");
+       }
+
+
+    // TODO 4: Query the database.
+      $query ="INSERT INTO catalogs VALUE ('$isbn','$author','$title','$price') ";
+      $result =$conn->query($query);
+
+    // TODO 5: Display the feedback back to user.
+       echo 'successful created';
+
+    // TODO 6: Disconnecting from the database.
+      $conn->close();
+
+    ?>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Search Results</title>
+</head>
+<body>
+    <h1>Book Search Results</h1>
+    <?php
+    // TODO 1: Create short variable names.
+     $lh='localhost';
+    $db = 'publications';
+    $un= 'sim';
+    $pw= 'sim';
+
+    // TODO 2: Check and filter data coming from the user.
+    if(isset($_POST['submit'])){
+        $searchtype = $_POST['searchtype'];
+        $searchterm = $_POST['searchterm'];
+    }else{
+        echo "unvalid data";
+    }
+
+    // TODO 3: Setup a connection to the appropriate database.
+    $conn = new mysqli($lh, $un, $pw, $db);
+    if($conn->connect_error){
+        die ("databese connection fail!");
+    }
+
+    // TODO 4: Query the database.
+    $query = "SELECT * FROM catalogs WHERE $searchtype LIKE '%$searchterm%'";
+
+    // TODO 5: Retrieve the results.
+    $result = $conn->query($query);
+    if(!$result){
+        die ("something went wrong");
+    }else{
+        echo "<h1>This is your search results!</h1>";
+    }
+    $rows = $result->num_rows;
+
+    // TODO 6: Display the results back to user.
+    echo "<table style='border: 1px solid black'>
+            <tr>
+                <th>isbn</th>
+                <th>author</th>
+                <th>title</th>
+                <th>price</th>
+            </tr>";
+
+        for($i=0; $i<$rows; $i++){
+            $row = $result->fetch_array(MYSQLI_NUM);
+            echo "<tr>";
+                for($j=0; $j<4; $j++){
+                    echo "<td>" . htmlspecialchars($row[$j]) . "</td>";
+
+                }
+            echo "</tr>";
+        }
+    echo "</table>";
+
+    // TODO 7: Disconnecting from the database.
+    $result->close();
+    $conn->close();
+
+    ?>
+</body>
+</html>
